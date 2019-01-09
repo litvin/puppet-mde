956d16eb7472ee74076ebd5ebfa45373e9beecb9
root <root@foreman.am.local>
2019-01-09 10:01:41 +0300

    add var ftp_server_name

--- manifests/downloads.pp
+++ manifests/downloads.pp
@@ -1,11 +1,12 @@
 class mde::downloads(
-	Array[String] $package_name_downloads  = $::mde::package_name_wget
+	Array[String] $package_name_downloads  = $::mde::package_name_wget,
+	String 	      $ftp_server_name	       = $::mde::ftp_server_name,
 ){
         include wget
 	
         $package_name_downloads.each | $index_download, $value_download | {
         wget::fetch { "$value_download":
-                      source             => "ftp://foreman.laba.lan/pub/install/$value_download",
+                      source             => "$ftp_server_name/$value_download",
                       destination        => "/tmp/$value_download",
                       timeout            => 0,
                       verbose            => true,
